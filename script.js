7aca6cad9060805e953de10bf189ac252b2ff13a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-01 12:58:54 -0300

    sla

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ sr.reveal(".apresentacao", {
   duration: 1000,
   delay: 500,
   origin: "left",
-  distance: "600px"
+  distance: "600px",
 });
 
 sr.reveal(".skills", {
   duration: 1000,
   delay: 500,
   origin: "right",
-  distance: "600px"
+  distance: "600px",
 });
 
 sr.reveal("#sobre", {
@@ -19,35 +19,39 @@ sr.reveal("#sobre", {
   rotate: {
     x: 0,
     y: 80,
-    z: 0
-  }
+    z: 0,
+  },
 });
 
 // Para os cards
 var cardConfig = {
   duration: 1000,
-  origin: 'left',
-  distance: '200px'
+  origin: "left",
+  distance: "200px",
 };
 
 var fillConfig = {
-    duration: 1000, 
-    origin: "left", 
-    distance: '600px'
+  duration: 1000,
+  origin: "left",
+  distance: "600px",
 };
 
 for (let i = 0; i < 10; i++) {
-    sr.reveal(`.fill${i}`, {
-        ...fillConfig,
-        delay: 700 + (i-1) * 70
-    });
-    
-}
-
-
-for(let i = 1; i <= 10; i++) {
-  sr.reveal(`#card_${i}`, {
-    ...cardConfig,
-    delay: 200 + (i-1) * 50  // Aqui estou incrementando o delay em 50ms para cada card
+  sr.reveal(`.fill${i}`, {
+    ...fillConfig,
+    delay: 700 + (i - 1) * 70,
   });
 }
+
+for (let i = 1; i <= 10; i++) {
+  sr.reveal(`#card_${i}`, {
+    ...cardConfig,
+    delay: 200 + (i - 1) * 50,
+  });
+}
+
+let galeria = document.getElementById("galeria");
+
+galeria.addEventListener("mouseenter", () => {
+  sr.reveal(".galeria", { duration: 1000 });
+});
