f68bcc65a4aa1f1afc43251f4ea4f2a1abaaab33
Pedreca <arthurpedreca@gmail.com>
2023-11-02 01:22:15 -0300

    "finalizado"

--- script.js
+++ script.js
@@ -50,8 +50,61 @@ for (let i = 1; i <= 10; i++) {
   });
 }
 
-let galeria = document.getElementById("galeria");
 
-galeria.addEventListener("mouseenter", () => {
-  sr.reveal(".galeria", { duration: 1000 });
+const galerias = document.querySelectorAll(".galeria");
+
+galerias.forEach(galeria => {
+  galeria.addEventListener("mouseenter", function () {
+    let texto = galeria.querySelector(".texto");
+    let ver_site = galeria.querySelector(".ver_site");
+
+    texto.style.opacity = "1";
+    texto.style.transform = "translateY(0px)";
+
+    ver_site.style.opacity = "1";
+    ver_site.style.transform = "translateY(0px)";
+  });
+
+  galeria.addEventListener("mouseleave", function () {
+    let texto = galeria.querySelector(".texto");
+    let ver_site = galeria.querySelector(".ver_site");
+
+    texto.style.opacity = "0";
+    texto.style.transform = "translateY(-300px)";
+
+    ver_site.style.opacity = "0";
+    ver_site.style.transform = "translateY(300px)";
+  });
 });
+
+sr.reveal('.trabalhos', {
+  duration: 1000,
+  delay: 500,
+  distance: '100px',
+  rotate: {
+    x: 0,
+    y: 50,
+    z: 0
+  }
+})
+
+sr.reveal('.custom-form', {
+  duration: 1000,
+  scale: 0.3
+})
+
+sr.reveal('.pergunta', {
+  duration: 1000,
+  origin: 'right',
+  distance: '500px'
+})
+
+sr.reveal('.fab', {
+  duration: 500,
+  distance: '300px',
+  origin: 'top'
+})
+
+
+
+
